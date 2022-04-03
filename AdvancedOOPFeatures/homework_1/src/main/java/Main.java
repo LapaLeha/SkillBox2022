2fb8933e32c0a047aca5e018714e3142874a4127
Aleksey Lapach <lapach2015@mail.ru>
2022-04-03 23:09:46 +0300

    homeWork 13.2 done

--- AdvancedOOPFeatures/homework_1/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_1/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -7,9 +8,22 @@ public class Main {
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         System.out.println(staff);
+        for (Employee e:staff){
+            System.out.println(e);
+        }
+        System.out.println("__________________");
+        sortBySalaryAndAlphabet(staff);
+        for (Employee e:staff){
+            System.out.println(e);
+        }
     }
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+        Collections.sort(staff,(o1, o2) -> {
+            if (o1.getSalary().equals(o2.getSalary())){
+                return o1.getName().compareTo(o2.getName());
+            }
+            return o1.getSalary().compareTo(o2.getSalary());
+        });
     }
 }
