a25ce55e02da7b3ca56eb360407e0674cedd1fab
Aleksey Lapach <lapach2015@mail.ru>
2022-04-05 22:08:00 +0300

    homeWork 13.8 exercise 1 done

--- AdvancedOOPFeatures/homework_2/Employees/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Employees/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.time.ZoneId;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -8,11 +10,17 @@ public class Main {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
         Employee employeeMaxSalary = findEmployeeWithHighestSalary(staff, 2017);
         System.out.println(employeeMaxSalary);
+
     }
 
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
-        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
-        // кто пришёл в году, указанном в переменной year
-        return null;
+        return staff.stream().filter(employee1 -> employee1.getWorkStart().toInstant()
+                        .atZone(ZoneId.systemDefault()).toLocalDate().getYear() == year)
+                .max(Comparator.comparing(Employee::getSalary)).get();
     }
+    // Вариант 2
+ /*      public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
+        return staff.stream().filter(employee1 -> employee1.getWorkStart().getYear()==(year-1900))
+                .max(Comparator.comparing(Employee::getSalary)).get();
+    }*/
 }
