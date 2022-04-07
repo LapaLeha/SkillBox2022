defc6f017ef80f0909078e5793686816e3c9039a
Aleksey Lapach <lapach2015@mail.ru>
2022-04-07 23:24:16 +0300

    homeWork 13.8 exercise 2 done

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,37 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import com.skillbox.airport.Terminal;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        Airport airport=Airport.getInstance();
+        airport.getAllAircrafts();
+        for (Flight f:findPlanesLeavingInTheNextTwoHours(airport)){
+            System.out.println(f);
+        }
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
 
+       List<Flight> flights=new ArrayList<>();
+        Date date=new Date();
+        long d = date.getHours();
+        airport.getTerminals().forEach(terminal -> terminal.getFlights().stream()
+                .filter(flight -> (flight.getType()== Flight.Type.ARRIVAL)&&(flight.getDate().getHours()>=(d-2))));
+
+        for (Terminal t:airport.getTerminals()){
+            for (Flight flight:t.getFlights()){
+                if (flight.getType()== Flight.Type.DEPARTURE){
+                    if (flight.getDate().getHours()>=d-2){
+                        flights.add(flight);
+                    }
+                }
+            }
+        }
+        return flights;
+    }
 }
