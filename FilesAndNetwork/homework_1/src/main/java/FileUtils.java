8e8e6133c2e8b72ca4ac6492b25cc5a366db2081
Aleksey Lapach <lapach2015@mail.ru>
2022-04-20 22:57:54 +0300

    HomeWork 15.5 done

--- FilesAndNetwork/homework_1/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_1/src/main/java/FileUtils.java
@@ -1,6 +1,31 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileUtils {
 
+    private static boolean error = false;
+
+    public static boolean isError() {
+        return error;
+    }
+
     public static long calculateFolderSize(String path) {
-        return 0;
+        error = false;
+        long result = 0;
+        try {
+            final File folder = new File(path);
+            for (final File file : folder.listFiles()) {
+                if (file.isDirectory()) {
+                    result += calculateFolderSize(file.getAbsolutePath());
+                } else {
+                    result += Files.size(Path.of(file.getPath()));
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            error = true;
+        }
+        return result;
     }
 }
--- FilesAndNetwork/homework_1/src/main/java/Main.java
+++ FilesAndNetwork/homework_1/src/main/java/Main.java
@@ -1,5 +1,50 @@
-public class Main {
-    public static void main(String[] args) {
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
+public class Main {
+    private static final long K = 1024;
+    private static final long M = K * K;
+    private static final long G = M * K;
+    private static final long T = G * K;
+
+    public static String convertToStringRepresentation(final long value) {
+        final long[] dividers = new long[]{T, G, M, K, 1};
+        final String[] units = new String[]{"TB", "GB", "MB", "KB", "B"};
+        if (value < 1)
+            throw new IllegalArgumentException("Invalid file size: " + value);
+        String result = null;
+        for (int i = 0; i < dividers.length; i++) {
+            final long divider = dividers[i];
+            if (value >= divider) {
+                result = format(value, divider, units[i]);
+                break;
+            }
+        }
+        return result;
+    }
+
+    private static String format(final long value,
+                                 final long divider,
+                                 final String unit) {
+        final double result =
+                divider > 1 ? (double) value / (double) divider : (double) value;
+        return new DecimalFormat("#,##0.#").format(result) + " " + unit;
+    }
+
+    public static void main(String[] args) {
+        //"D:/Леша/Обучение/Java/m12"
+
+        System.out.print("Введите путь до папки:\n");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            long size = FileUtils.calculateFolderSize(input);
+            if (FileUtils.isError()) {
+                System.out.println("Ошибка ввода");
+                continue;
+            }
+            System.out.println("Размер папки " + input + " cоставляет "
+                    + convertToStringRepresentation(size));
+        }
     }
 }
