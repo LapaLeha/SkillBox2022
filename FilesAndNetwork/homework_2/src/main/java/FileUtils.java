8559150761be9d16635838c6faf673f4f37be55b
Aleksey Lapach <lapach2015@mail.ru>
2022-04-23 19:30:24 +0300

    homeWork 15.7 done

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,27 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+    public static void copyFolder(String sourceDirectory, String destinationDirectory){
+
+        try {
+            final File folder = new File(sourceDirectory);
+            for (final File file : folder.listFiles()) {
+                if (file.isDirectory()) {
+                    Files.createDirectory(Path.of(destinationDirectory + "/" + file.getName()));
+                    copyFolder(file.getPath(), (destinationDirectory + "/" + file.getName()));
+                } else if (file.isFile()) {
+                    byte[] fileCopy = Files.readAllBytes(file.toPath());
+                    FileOutputStream fileOutputStream = new FileOutputStream(destinationDirectory+
+                            "/"+file.getName());
+                    fileOutputStream.write(fileCopy);
+                    fileOutputStream.close();
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        while (true) {
+            System.out.print("Введите путь копируемой папки:\n");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            System.out.print("Введите путь к папкe для вставки:\n");
+            Scanner scanner1 = new Scanner(System.in);
+            String input1 = scanner1.nextLine();
+
+            FileUtils.copyFolder(input, input1);
+
+        }
     }
 }
