3712d1296ef90df82daabde93ba81f7b3127c530
Aleksey Lapach <lapach2015@mail.ru>
2022-04-28 17:58:42 +0300

    homeWork 15.9 done

--- FilesAndNetwork/homework_3/src/main/java/Main.java
+++ FilesAndNetwork/homework_3/src/main/java/Main.java
@@ -1,4 +1,19 @@
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        Movements movements = new Movements("data/movementList.csv");
+        movements.parse();
+        System.out.print("Сумма расходов: ");
+        System.out.println(movements.getExpenseSum());
+        System.out.print("Сумма доходов: ");
+        System.out.println(movements.getIncomeSum());
+        System.out.println();
+        System.out.println("Суммы расходов по организациям:");
+        Set<String> company = movements.getCompanyOfExpense();
+
+        for (String c:company){
+            System.out.println(c);
+        }
     }
 }
--- FilesAndNetwork/homework_3/src/main/java/Movements.java
+++ FilesAndNetwork/homework_3/src/main/java/Movements.java
@@ -1,13 +1,81 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
 public class Movements {
+    String pathMovementsCsv;
+    private final List<Double> income = new ArrayList<>();
+    private final List<Double> expense = new ArrayList<>();
+    private final Map<String, Double> companyOfExpense = new HashMap<>();
 
     public Movements(String pathMovementsCsv) {
+        this.pathMovementsCsv = pathMovementsCsv;
+        parse();
+        getCompanyOfExpense();
+    }
+
+    public void parse() {
+        try {
+            List<String> lines = Files.readAllLines(Path.of(pathMovementsCsv));
+            for (int i = 0; i < lines.size(); i++) {
+                String[] fragments = lines.get(i).split(",(?=(?:[^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)");
+                if (lines.get(i).startsWith("Тип")) {
+                    continue;
+                }
+
+                income.add(Double.parseDouble(fragments[6].replace(",", ".")
+                        .replace("\"", " ")));
+
+                double expense1 = Double.parseDouble(fragments[7]
+                        .replace(",", ".").replace("\"", ""));
+                expense.add(expense1);
+
+                if (fragments.length != 8) {
+                    System.out.println("Error:" + lines.get(i));
+                    continue;
+                }
+
+                if (expense1!=0) {
+                    String key;
+                    if (!fragments[5].contains("\\")) {
+                        key = fragments[5].substring(fragments[5].indexOf("/")).substring(0, fragments[5].indexOf("/"));
+                    } else {
+                        key = fragments[5].substring(fragments[5].indexOf("\\")).substring(0, fragments[5].indexOf("\\"));
+                    }
+                    if (companyOfExpense.containsKey(key)) {
+                        companyOfExpense.put(key, companyOfExpense.get(key) + expense1);
+                    } else {
+                        companyOfExpense.put(key, expense1);
+                    }
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public double getExpenseSum() {
-        return 0.0;
+        double expenseSum = 0.0;
+        for (Double e : expense) {
+            expenseSum += e;
+        }
+        return expenseSum;
     }
 
     public double getIncomeSum() {
-        return 0.0;
+        double incomeSum = 0.0;
+        for (Double i : income) {
+            incomeSum += i;
+        }
+        return incomeSum;
+    }
+
+    public Set<String> getCompanyOfExpense() {
+        TreeSet<String> allCompany = new TreeSet<>();
+        for (String key : companyOfExpense.keySet()) {
+            allCompany.add(key + "\t" + companyOfExpense.get(key));
+        }
+        return allCompany;
     }
 }
