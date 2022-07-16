3b8fe9862be78ed2cca2d70ed45a1c6584f1cc9d
Aleksey Lapach <lapach2015@mail.ru>
2022-07-16 13:53:08 +0300

    HomeWork 22.7 done

--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,41 +1,45 @@
-import java.io.FileOutputStream;
 
 public class Loader {
 
     public static void main(String[] args) throws Exception {
+        String path1 = "res/numbers1.txt";
+        String path2 = "res/numbers2.txt";
+        String path3 = "res/numbers3.txt";
+        String path4 = "res/numbers4.txt";
+        int processorHeads = 4;
+        int regionFrom1 = 1;
+        int regionTo1 = 100 / processorHeads;
+        int regionFrom2 = regionTo1;
+        int regionTo2 = regionTo1 + 100 / processorHeads;
+        int regionFrom3 = regionTo2;
+        int regionTo3 = regionTo2 + 100 / processorHeads;
+        int regionFrom4 = regionTo3;
+        int regionTo4 = 100;
+
+        int numberFrom1 = 1;
+        int numberTo1 = 1000 / processorHeads;
+        int numberFrom2 = numberTo1;
+        int numberTo2 = numberTo1 + 1000 / processorHeads;
+        int numberFrom3 = numberTo2;
+        int numberTo3 = numberTo2 + 1000 / processorHeads;
+        int numberFrom4 = numberTo3;
+        int numberTo4 = 1000;
+
         long start = System.currentTimeMillis();
+        MakeGenerationNumbers generator1
+                = new MakeGenerationNumbers(path1, start, regionFrom1, regionTo1, numberFrom1, numberTo1);
+        generator1.start();
 
-        FileOutputStream writer = new FileOutputStream("res/numbers.txt");
+        MakeGenerationNumbers generator2
+                = new MakeGenerationNumbers(path2, start, regionFrom2, regionTo2, numberFrom2, numberTo2);
+        generator2.start();
 
-        char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
-                    }
-                }
-            }
-        }
+        MakeGenerationNumbers generator3
+                = new MakeGenerationNumbers(path3, start, regionFrom3, regionTo3, numberFrom3, numberTo3);
+        generator3.start();
 
-        writer.flush();
-        writer.close();
-
-        System.out.println((System.currentTimeMillis() - start) + " ms");
-    }
-
-    private static String padNumber(int number, int numberLength) {
-        String numberStr = Integer.toString(number);
-        int padSize = numberLength - numberStr.length();
-
-        for (int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
-        }
-
-        return numberStr;
+        MakeGenerationNumbers generator4
+                = new MakeGenerationNumbers(path4, start, regionFrom4, regionTo4, numberFrom4, numberTo4);
+        generator4.start();
     }
 }
---
+++ Performance/CarNumberGenerator/src/MakeGenerationNumbers.java
@@ -0,0 +1,67 @@
+import java.io.PrintWriter;
+
+public class MakeGenerationNumbers extends Thread{
+
+    private String dstFolder;
+    private long start;
+    private int regionFrom;
+    private int regionTo;
+    private int numberFrom;
+    private int numberTo;
+
+    public MakeGenerationNumbers(String dstFolder, long start, int regionFrom,
+                                 int regionTo, int numberFrom, int numberTo) {
+        this.dstFolder = dstFolder;
+        this.start = start;
+        this.regionFrom = regionFrom;
+        this.regionTo = regionTo;
+        this.numberFrom = numberFrom;
+        this.numberTo = numberTo;
+    }
+
+    @Override
+    public void run() {
+        try {
+            PrintWriter writer = new PrintWriter(dstFolder);
+            char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
+            for (int regionCode = regionFrom; regionCode < regionTo; regionCode++) {
+                StringBuilder builder = new StringBuilder();
+                for (int number = numberFrom; number < numberTo; number++) {
+                    for (char firstLetter : letters) {
+                        for (char secondLetter : letters) {
+                            for (char thirdLetter : letters) {
+                                builder.append(firstLetter);
+                                builder.append(padNumber(number, 3));
+                                builder.append(secondLetter);
+                                builder.append(thirdLetter);
+                                builder.append(padNumber(regionCode, 2));
+                                builder.append("\n");
+                            }
+                        }
+                    }
+                }
+                writer.write(builder.toString());
+            }
+            writer.flush();
+            writer.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Finished after start: " + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    private String padNumber(int number, int numberLength) {
+        String numberStr = Integer.toString(number);
+        int padSize = numberLength - numberStr.length();
+        StringBuilder builder2 = new StringBuilder();
+        for (int i = 0; i <= padSize; i++) {
+            if (i!=padSize){
+                builder2.append('0');
+            }else{
+                builder2.append(numberStr);
+            }
+        }
+        numberStr = builder2.toString();
+        return numberStr;
+    }
+}
